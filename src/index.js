de04407c3b88edd9657d4f5a5adbd03a251fc7ef
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-11-04 09:45:58 +0100

    connectie met database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+let result;
 async function main() {
   const mysql = require('mysql2/promise');
 
@@ -10,9 +11,25 @@ async function main() {
     connectionLimit: 10,
   });
 
-  const [result] = await pool.query('SELECT * from films;');
-
+  [result] = await pool.query(`select * from films;`);
   console.log(result);
 }
 
+// INSERT INTO films(Title)
+//         // VALUES ('thor, The dark world')
 main();
+
+// const Koa = require('koa');
+// const app = new Koa();
+
+// app.use(async (ctx, next) => {
+//   ctx.body = result;
+//   await next();
+// });
+
+// app.use(async (ctx, next) => {
+//   console.log(ctx);
+//   await next();
+// });
+
+// app.listen(9000);
