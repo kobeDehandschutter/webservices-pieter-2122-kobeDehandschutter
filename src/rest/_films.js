ac086e413c94eb25e064f7155957c545685e5be0
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-12-14 19:06:16 +0100

    apiGelukt

--- src/rest/_films.js
+++ src/rest/_films.js
@@ -5,24 +5,6 @@ const getAllfilms = async (ctx) => {
   ctx.body = await filmService.getAll();
 };
 
-const createfilm = async (ctx) => {
-  const newfilm = await filmService.create(ctx.request.body);
-  ctx.body = newfilm;
-};
-
-const getfilmById = async (ctx) => {
-  ctx.body = await filmService.getById(ctx.params.id);
-};
-
-const updatefilm = async (ctx) => {
-  ctx.body = await filmService.updateById(ctx.params.id, ctx.request.body);
-};
-
-const deletefilm = async (ctx) => {
-  await filmService.deleteById(ctx.params.id);
-  ctx.status = 204;
-};
-
 /**
  * Install transaction routes in the given router.
  *
@@ -34,10 +16,6 @@ module.exports = (app) => {
   });
 
   router.get('/', getAllfilms);
-  router.post('/', createfilm);
-  router.get('/:id', getfilmById);
-  router.put('/:id', updatefilm);
-  router.delete('/:id', deletefilm);
 
   app.use(router.routes()).use(router.allowedMethods());
 };
--- src/service/film.js
+++ src/service/film.js
@@ -22,61 +22,9 @@ const getAll = async (
 ) => {
   debugLog('Fetching all films', { limit, offset });
   const data = await filmRepository.findAll({ limit, offset });
-  const count = await filmRepository.findCount();
-  return { data, count };
-};
-
-/**
- * Get the place with the given `id`.
- *
- * @param {string} id - Id of the place to get.
- */
-const getById = (id) => {
-  debugLog(`Fetching film with id ${id}`);
-  return filmRepository.findById(id);
-};
-
-/**
- * Create a new place.
- *
- * @param {object} film - Place to create.
- * @param {string} film.name - Name of the place.
- * @param {number} [film.rating] - Rating of the place (between 1 and 5).
- */
-const create = ({ name, rating }) => {
-  const newfilm = { name, rating };
-  debugLog('Creating new film', newfilm);
-  return filmRepository.create(newfilm);
-};
-
-/**
- * Update an existing place.
- *
- * @param {string} id - Id of the place to update.
- * @param {object} film - Place to save.
- * @param {string} [film.name] - Name of the place.
- * @param {number} [film.rating] - Rating of the place (between 1 and 5).
- */
-const updateById = (id, { name, rating }) => {
-  const updatedfilm = { name, rating };
-  debugLog(`Updating film with id ${id}`, updatedfilm);
-  return filmRepository.updateById(id, updatedfilm);
-};
-
-/**
- * Delete an existing place.
- *
- * @param {string} id - Id of the place to delete.
- */
-const deleteById = async (id) => {
-  debugLog(`Deleting film with id ${id}`);
-  await filmRepository.deleteById(id);
+  return { data };
 };
 
 module.exports = {
   getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
 };
