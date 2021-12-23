cc1caeef209f9b44d09c84d021b80ef2bc5897cd
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-12-23 23:55:10 +0100

    CORS

--- config/production.js
+++ config/production.js
@@ -7,6 +7,7 @@ module.exports = {
     origins: [
       'http://localhost:3000',
       'https://kobedehandschutter.github.io/frontendweb-karine-2122-kobeDehandschutter/',
+      'https://kobedehandschutter.github.io',
     ],
     maxAge: 3 * 60 * 60,
   } /*
